7cc9fa836cd24206420cc5d320d062aaa14a55dc
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-21 15:31:29 +0100

    feat(): add code in create-article.dto

--- src/article/dto/create-article.dto.ts
+++ src/article/dto/create-article.dto.ts
@@ -1,1 +1,67 @@
-export class CreateArticleDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateArticleDto {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  titre: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  date_creation: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  date_suppression: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  url_img: string;
+
+  @IsNotEmpty()
+  @IsString()
+  img_description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  img_titre: string;
+
+  @IsNotEmpty()
+  @IsString()
+  sous_titre: string;
+
+  @IsNotEmpty()
+  @IsString()
+  contenu: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  meta_title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  categories: string;
+
+  @IsNotEmpty()
+  @IsString()
+  url_video: string;
+
+  @IsNotEmpty()
+  @IsString()
+  slug: string;
+
+  @IsNotEmpty()
+  @IsString()
+  tags: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+}
