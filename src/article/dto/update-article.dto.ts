1120506449a5fb822dd765c6d0ac239d97f0378d
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-21 15:54:49 +0100

    feat(): modification class UpdateArticleDto in update-article.dto

--- src/article/dto/update-article.dto.ts
+++ src/article/dto/update-article.dto.ts
@@ -1,4 +1,65 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 import { CreateArticleDto } from './create-article.dto';
 
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
+export class UpdateArticleDto extends PartialType(CreateArticleDto) {
+  @IsNotEmpty()
+  @IsString()
+  titre: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  date_creation: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  date_suppression: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  url_img: string;
+
+  @IsNotEmpty()
+  @IsString()
+  img_description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  img_titre: string;
+
+  @IsNotEmpty()
+  @IsString()
+  sous_titre: string;
+
+  @IsNotEmpty()
+  @IsString()
+  contenu: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  meta_title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  categories: string;
+
+  @IsNotEmpty()
+  @IsString()
+  url_video: string;
+
+  @IsNotEmpty()
+  @IsString()
+  slug: string;
+
+  @IsNotEmpty()
+  @IsString()
+  tags: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+}
