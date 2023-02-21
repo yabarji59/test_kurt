b80d7d42f1277da9c55c14f33678e52c993d4c13
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-21 14:52:50 +0100

    feat(): create CRUD article

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UtilisateursModule } from './utilisateurs/utilisateurs.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PagesModule } from './pages/pages.module';
 
 @Module({
-  imports: [UtilisateursModule, PagesModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
