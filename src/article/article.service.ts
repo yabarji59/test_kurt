0957e0771649bcb20b579371e20fec22f0125b00
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-22 14:27:39 +0100

    feat: modification  create, findAll, findOne, update and remove into class ArticleService

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createArticleDto: CreateArticleDto) {
+    return await this.prisma.post.create({
+      data: {
+        ...createArticleDto,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all article`;
+    return this.prisma.post.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.post.findUnique({
+      where: { id: id },
+    });
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.post.update({
+      where: { id: id },
+      data: {
+        ...updateArticleDto,
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.post.delete({
+      where: { id: id },
+    });
   }
 }
