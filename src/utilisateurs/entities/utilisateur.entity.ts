d0b46781d5a0668cd01d458b29cddfb61e187ef5
nathancuvelier <nathancuvelier59@icloud.com>
2023-02-23 09:58:16 +0100

    test: add test for utilisateur , add db_test to test into a test database

--- src/utilisateurs/entities/utilisateur.entity.ts
+++ src/utilisateurs/entities/utilisateur.entity.ts
@@ -1,1 +1,29 @@
-export class Utilisateur {}
+import { Column, PrimaryGeneratedColumn } from 'prisma';
+export class Utilisateur {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  prenom: string;
+
+  @Column()
+  nom: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  mot_de_passe: string;
+
+  @Column()
+  uuid: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  date_creation: Date;
+
+  @Column()
+  date_modification: Date;
+}
