af1e57083a2a25c52be7e0cc0ce92b2bae1688dc
Josue <96113041+Rowada@users.noreply.github.com>
2023-02-23 09:50:22 +0100

    feat: update schema.prisma

--- src/pages/dto/create-page.dto.ts
+++ src/pages/dto/create-page.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePageDto {}
+import { ApiProperty} from "@nestjs/swagger";
+
+
+export class CreatePageDto {
+    uuid: string;
+    content: string;
+    image: string;
+    linkUrl: string;
+
+}
